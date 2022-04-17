aa1af106e403dfd4e957332cc8f6fb89db4e9400
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2022-04-17 21:11:53 +1200

    Update NewGame.java
    Removed colours because of netbeans being a shit cunt
    Added load game option.

--- Assignment 1/src/com/buffalo/thevoid/main/NewGame.java
+++ Assignment 1/src/com/buffalo/thevoid/main/NewGame.java
@@ -4,10 +4,8 @@ import com.buffalo.thevoid.data.Config;
 import com.buffalo.thevoid.entity.Player;
 import com.buffalo.thevoid.equipment.Weapon;
 import com.buffalo.thevoid.equipment.WeaponList;
-import com.buffalo.thevoid.io.ConsoleColours;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.util.Scanner;
 
@@ -15,7 +13,7 @@ import java.util.Scanner;
 // Relies on fileIO to read the config file which is then loaded into the config class.
 class NewGame
 {
-    // Get the first item regardless of whether it's been set to unlocked or not.
+    // Get the first weapon regardless of whether it's been set to unlocked or not.
     private static final Weapon defaultWeapon = WeaponList.WeaponData.get(0).getItem1();
 
     public static Player newPlayer()
@@ -23,7 +21,7 @@ class NewGame
         String name;
         Scanner s = new Scanner(System.in);
 
-        System.out.printf("%sWhat is your name?%s ", ConsoleColours.ANSI_BLACK_BACKGROUND, ConsoleColours.TEXT_RESET);
+        System.out.println("What is your name? ");
         name = s.nextLine();
 
 //        This was funny the first 10 times and now it's just annoying.
@@ -46,7 +44,7 @@ class NewGame
     {
         // If any of these variables are not set, then the catch will be triggered.
         String name = "";
-        int HP = 0, MP = 0, DEF = 0, RES = 0, Level = 0, Kills = 0;
+        int HP = 0, MP = 0, DEF = 0, RES = 0, Level = 0, Kills = 0, WeaponsUnlocked = 0;
 
         try(BufferedReader br = new BufferedReader(new FileReader(Config.cwd + "/resources/save/player.dat")))
         {
@@ -63,14 +61,21 @@ class NewGame
                     case "RES" -> RES = Integer.parseInt(split[1]);
                     case "Level" -> Level = Integer.parseInt(split[1]);
                     case "KillCount" -> Kills = Integer.parseInt(split[1]);
+                    case "WeaponsUnlocked" -> WeaponsUnlocked = Integer.parseInt(split[1]);
                 }
             }
 
-            return new Player(name, HP, MP, DEF, RES, defaultWeapon, Kills, Level);
+            // Unlock weapons
+            for(int x = 0; x < WeaponsUnlocked; x++)
+            {
+                WeaponList.unlockWeapon();
+            }
+
+            return new Player(name, HP, MP, DEF, RES, Kills, Level, Config.KILLS_TO_LEVEL_UP, defaultWeapon);
         }
         catch(Exception e)
         {
-            System.out.println("Error loading player data.");
+            System.out.println("Error loading player data. Check player.dat");
             return null;
         }
     }
