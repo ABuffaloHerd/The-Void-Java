6c359a72f80f5ed20cd2805547a08a9b4de8cdaa
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2022-04-24 14:36:47 +1200

    Update Spell.java
    Now with failrate!

--- Assignment 1/src/com/buffalo/thevoid/equipment/Spell.java
+++ Assignment 1/src/com/buffalo/thevoid/equipment/Spell.java
@@ -6,16 +6,13 @@ import java.util.function.Function;
 
 public class Spell extends AbstractWeapon
 {
-    public final int cost;
+    public final int cost; // This is all the game needs to know about a spell. the game will handle spell checking and mana consumption.
+    public final float failrate; // You know what's funny? Spells blowing up in your face.
 
-    public Spell(final String name, Function<IEntity, Integer> useMethod, final int levelRequired, final int cost)
+    public Spell(final String name, Function<IEntity, Integer> useMethod, final int levelRequired, final int cost, final float failrate)
     {
         super(name, useMethod, levelRequired);
         this.cost = cost;
-    }
-
-    public int cast(IEntity entity)
-    {
-        return standardAttackMethod.apply(entity);
+        this.failrate = failrate;
     }
 }
