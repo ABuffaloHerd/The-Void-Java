4332c249d177f2a6bf5aa1a99e1b02f943109bd3
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2022-05-07 11:43:46 +1200

    Update gitignore

--- TheVoid/src/main/java/com/buffalo/thevoid/event/IEventPublisher.java
+++ TheVoid/src/main/java/com/buffalo/thevoid/event/IEventPublisher.java
@@ -2,6 +2,7 @@ package com.buffalo.thevoid.event;
 
 // <?> means ieventhandler uses a generic class
 // raise isn't part of this interface because it's up to the implementing class to decide how to raise events.
+// This will be the godsend to make guis work.
 public interface IEventPublisher
 {
     void addEventHandler(IEventHandler<?> e);
