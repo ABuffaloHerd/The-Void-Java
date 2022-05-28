8f3c65c31644c4085d24f11e801d6bedbefe5cdc
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2022-05-28 20:43:26 +1200

    Embedded db seems to be working dunno about shitfuck ide

---
+++ TheVoid/src/main/java/com/buffalo/thevoid/db/FuckYou.java
@@ -0,0 +1,12 @@
+package com.buffalo.thevoid.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class FuckYou
+{
+
+}
